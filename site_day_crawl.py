dac3ab6f6321f12824343f34fb281fe5b30285d4
andyfcx <dreamcastle.andy@gmail.com>
2019-05-22 01:56:57 +0800

    add dates file

--- site_day_crawl.py
+++ site_day_crawl.py
@@ -1,10 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.parse import urlencode, quote_plus
+# %E7%9F%B3%E7%89%8C
+# C0A9B0_石牌
+payload = {'station':'C0A9B0', 'stname':'石牌','datepicker':'2018-06-23'}
+result = urlencode(payload, quote_via=quote_plus)
 
-url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=C0A9C0&stname=%25E5%25A4%25A9%25E6%25AF%258D&datepicker=2018-05-19#"
+url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&{}".format(result)
+
+# url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=C0A9C0&stname=%25E5%25A4%25A9%25E6%25AF%258D&datepicker=2018-05-19#"
 
 def fetch(url):
-    res = requests.get(url)
+    res = requests.post(url) #可能要用request post form data
     soup = BeautifulSoup(res.text, 'lxml')
     return soup
 
@@ -30,4 +37,8 @@ col_name_en = [name.string for name in soup.select("tr.second_tr")[1] if name.st
 
 raw_data = soup.select("tr")[-24:]
 
+
+# panel data
+# station id 
+# date, station name
 
