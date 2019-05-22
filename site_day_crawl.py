71cd033566eb00a9918fa7e50ff4eab3282dd8dc
andyfcx <dreamcastle.andy@gmail.com>
2019-05-22 22:06:49 +0800

    replace % -> %25

--- site_day_crawl.py
+++ site_day_crawl.py
@@ -1,14 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.parse import urlencode, quote_plus
-# %E7%9F%B3%E7%89%8C
-# C0A9B0_石牌
+import csv
+
+# https://e-service.cwb.gov.tw/HistoryDataQuery/index.jsp
 payload = {'station':'C0A9B0', 'stname':'石牌','datepicker':'2018-06-23'}
 result = urlencode(payload, quote_via=quote_plus)
+# url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&{}".format(result)
 
-url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&{}".format(result)
-
-# url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=C0A9C0&stname=%25E5%25A4%25A9%25E6%25AF%258D&datepicker=2018-05-19#"
+url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&station=C0A9C0&stname=%25E5%25A4%25A9%25E6%25AF%258D&datepicker=2018-05-19#"
 
 def fetch(url):
     res = requests.post(url) #可能要用request post form data
@@ -28,6 +28,13 @@ def parse_data(item):
             finally:
                 array.append(c)
     return array
+# Read dates
+with open('date2018') as f:
+    dates = f.read().split('\n')
+# Read sites
+with open('sites') as f:
+    sites = [{k: v for k, v in row.items()}
+        for row in csv.DictReader(f, skipinitialspace=True)]
 
 soup = fetch(url)
 
@@ -37,8 +44,18 @@ col_name_en = [name.string for name in soup.select("tr.second_tr")[1] if name.st
 
 raw_data = soup.select("tr")[-24:]
 
-
 # panel data
 # station id 
 # date, station name
 
+for site in sites[:5]:
+    for date in date[:5]:
+        payload = {'station':site['station'], 'stname':site['stname'],'datepicker':date}
+        result = urlencode(payload, quote_via=quote_plus)
+        url = "https://e-service.cwb.gov.tw/HistoryDataQuery/DayDataController.do?command=viewMain&{}".format(result).replace('%','%25') 
+        res = requests.get(url)
+        if 'SeaPres' in res.text:
+            print("Success!")
+        else:
+            print("XXX")
+               
